e4408f6f3a6019587bc50ba9b3904dba411bfbea
Anjali Sharma <107831444+Anjali-sharma1313@users.noreply.github.com>
2023-08-31 14:10:33 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,7 @@ export default function HomePage() {
         const data = await contract.name();
         console.log("data: ", data);
         setCurrentNaming(data);
+        // document.getElementById("app").style.backgroundColor=data;
       } catch (error) {
         console.log("Error: ", error);
       }
@@ -53,7 +54,7 @@ export default function HomePage() {
   
       try {
         const transaction = await contract.setNaming(message);
-        setMessage("");
+        setMessage(" ");
         await transaction.wait();
         fetchNaming();
       } catch (error) {
@@ -64,76 +65,8 @@ export default function HomePage() {
   
 
   return (
-  //   <div className="App" style={{
-  //     backgroundColor:"gray",
-  //     color:"black",
-  //     borderRadius:"40px",
-  //     display:"flex",
-  //     justifyContent: "center",
-  //     height:"95vh",
-  //     maxHeight: "95vh",
-  //     padding: "20px",
-  //   }}>
-  //     <div className="App-header">
-  //       {/* DESCRIPTION  */}
-  //       <div className="description">
-  //         <h1>Hello, Crafter!</h1>
-  //         <h2>Smart Contract Management</h2>
-  //         <h3>Project: Function Frontend</h3>
-  //       </div>
-  //       {/* BUTTONS - Fetch and Set */}
-  //       <div className="custom-buttons">
-  //         <button onClick={fetchNaming} style={{ 
-  //           padding:"20px",
-  //           border:"3px black solid",
-  //           borderRadius:"10px",
-  //           marginBottom:"10px",
-  //           marginRight:"20px",
-  //           color:"black",
-  //           }}>
-  //           Fetch Names
-  //         </button>
-  //         <button onClick={setNaming} style={{ 
-  //           backgroundColor: "black",
-  //           border:"3px white solid",
-  //           padding:"20px",
-  //           borderRadius:"10px",
-  //           marginBottom:"10px",
-  //           marginRight:"20px" ,
-  //           color:"white",
-  //           }}>
-  //           Set Names
-  //         </button>
-  //       </div>
-  //       {/* INPUT TEXT - String  */}
-  //       <input
-  //         onChange={(e) => setMessage(e.target.value)}
-  //         value={message}
-  //         placeholder="Set Naming Message"
-  //         style={{
-  //           justifyContent: "center",
-  //           border:"3px black solid",
-  //           marginTop:"10px",
-  //           padding:"20px",
-  //           borderRadius:"10px",
-  //           width:"220px",
-  //         }}
-  //       />
 
-  //       {/* Current Value stored on Blockchain */}
-  //       <h2 className="naming" style={{
-  //         padding:"20px",
-  //         backgroundColor: "white",
-  //         color:"green",
-  //         border: "3px solid black",
-  //         borderRadius:"10px",
-  //         justifyContent: "center",
-  //       }}>Names: {currentNaming}</h2>
-  //     </div>
-  //   </div>
-  // );
-
-  <div className="App" style={{
+  <div className="App" id="app" style={{
     backgroundColor: "#f0f0f0",  // Change the background color
     color: "black",
     borderRadius: "40px",
@@ -147,9 +80,9 @@ export default function HomePage() {
     <div className="App-header">
         {/* DESCRIPTION  */}
         <div className="description" style={{ marginBottom: "20px" }}>
-            <h1>Book management system</h1>
-            <h2>Opt your books here</h2>
-            <h3>Project: Frontend using react</h3>
+            <h1>Metacrafter mobile store</h1>
+            <h2>Enter Mobile Brand</h2>
+            <h3>with model</h3>
         </div>
         {/* BUTTONS - Fetch and Set */}
         <div className="custom-buttons" style={{ display: "flex", justifyContent: "center" }}>
@@ -161,8 +94,9 @@ export default function HomePage() {
                 marginRight: "10px",  // Decrease right margin
                 color: "black",
             }}>
-                Fetch Book
+                Get Mobile Brand
             </button>
+            
             <button onClick={setNaming} style={{
                 margin:"20px",
                 backgroundColor: "black",
@@ -171,22 +105,25 @@ export default function HomePage() {
                 borderRadius: "10px",
                 color: "white",
             }}>
-                Set books Names
+                Put Mobile Model
             </button>
         </div>
         {/* INPUT TEXT - String  */}
         <input
             onChange={(e) => setMessage(e.target.value)}
             value={message}
-            placeholder="Enter name of book"
-            style={{
+            placeholder="Enter value"
+            style=
+            {
+              {
                 border: "5px black solid",
                 marginTop: "20px",
                 padding: "10px",  // Adjust input padding
                 borderRadius: "10px",
                 width: "100%",    // Use full width
                 boxSizing: "border-box",  // Include padding in width calculation
-            }}
+              }
+            }
         />
 
         {/* Current Value stored on Blockchain */}
@@ -197,7 +134,7 @@ export default function HomePage() {
             border: "3px solid black",
             borderRadius: "10px",
             textAlign: "center",  // Center align text
-        }}> Book Names: {currentNaming}</h2>
+        }}> Last entry: {currentNaming}</h2>
     </div>
 </div>
   );
